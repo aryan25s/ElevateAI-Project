c0088afcfe1dbe75620d7681f317d3916c1ffbe1
kartikf4 <kartikeyaprasad407@gmail.com>
2025-03-06 01:05:19 +0530

    all changes

--- src/pages/01_About.py
+++ src/pages/01_About.py
@@ -10,21 +10,7 @@ def main():
     # Your content
     st.title("Elevate.AI")
     st.header("Elevating businesses by leveraging LLMs")
-    
-    # CSS with relative path
-    def local_css(file_name):
-        with open(file_name) as f:
-            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-    local_css("D:/NewElevate.ai/Newelevateai/src/styles/style.css")  # Not absolute path!
-    
-    # Animation
-    animation_symbol = "❄"
-    st.markdown(
-        f"""
-        <div class="snowflake">{animation_symbol}</div>
-        """,
-        unsafe_allow_html=True,
-    )
+
 
 # REQUIRED: This makes the page work
 if __name__ == "__main__":
